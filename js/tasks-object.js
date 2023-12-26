5e69431811e1fd82a0e76e9b924511c6782bb47f
TyshchenkoYulia <veshkina.yulia021088@gmail.com>
2023-12-26 21:41:34 +0200

    add

--- js/tasks-object.js
+++ js/tasks-object.js
@@ -97,3 +97,123 @@
 // console.log(countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 }));  
 // ==========================================================================================
 
+// Масив colors містить колекцію кольорів. Кожен колір представлений об'єктом і 
+// має властивості hex і rgb з відповідними для цього формату і кольору значеннями.
+
+// Перебери масив об'єктів colors, використовуючи цикл for...of. 
+// Додай у масив hexColors значення властивостей hex,
+//  а в масив rgbColors - значення властивостей rgb з усіх об'єктів масиву colors.
+
+// const colors = [
+//     { hex: "#f44336", rgb: "244,67,54" },
+//     { hex: "#2196f3", rgb: "33,150,243" },
+//     { hex: "#4caf50", rgb: "76,175,80" },
+//     { hex: "#ffeb3b", rgb: "255,235,59" },
+//   ];
+  
+  
+//   const hexColors = [];
+//   const rgbColors = [];
+  
+//   for ( const color of colors) {
+//     hexColors.push(color.hex);
+//     rgbColors.push(color.rgb);
+//   }
+//   console.log(hexColors);
+//   console.log(rgbColors);
+
+// ============================================================================================
+// Функція getProductPrice(productName) приймає один параметр productName - назва продукту. 
+// Функція містить масив об'єктів products з такими властивостями, 
+// як name — ім'я товару, price — ціна і quantity — кількість.
+
+// Доповни код функції так, щоб вона шукала об'єкт продукту з певним ім'ям (властивість name) 
+// в масиві products і повертала його ціну (властивість price). Якщо продукт з такою назвою не знайдений, 
+// функція повинна повертати null.
+
+// function getProductPrice(productName) {
+//     const products = [
+//       { name: "Radar", price: 1300, quantity: 4 },
+//       { name: "Scanner", price: 2700, quantity: 3 },
+//       { name: "Droid", price: 400, quantity: 7 },
+//       { name: "Grip", price: 1200, quantity: 9 },
+//     ];
+  
+  
+//   for (let i = 0; i < products.length; i++) {
+//    if (products[i].name === productName) {
+//     return products[i].price;
+//    }
+   
+//   }
+//   return null;
+// }
+//   console.log(getProductPrice("Radar"));
+//   console.log(getProductPrice("Scanner"));
+//   console.log(getProductPrice("Droid"));
+//   console.log(getProductPrice("Grip"));
+//   console.log(getProductPrice("Engine"));
+  
+//   ===================================================================================================
+// Напиши функцію getAllPropValues(propName), яка приймає один параметр propName - ім'я (ключ) властивості.
+//  Функція повинна повернути масив усіх значень властивості з таким ім'ям з кожного об'єкта в масиві products. 
+//  Якщо в об'єктах відсутні властивості з таким ім'ям, 
+// функція повинна повернути порожній масив.
+
+// function getAllPropValues(propName) {
+//     const products = [
+//       { name: "Radar", price: 1300, quantity: 4 },
+//       { name: "Scanner", price: 2700, quantity: 3 },
+//       { name: "Droid", price: 400, quantity: 7 },
+//       { name: "Grip", price: 1200, quantity: 9 },
+//     ];
+
+// const array = [];
+// for (let i = 0; i < products.length; i++) {
+//     if (products[i].hasOwnProperty(propName)) {
+//          array.push(products[i][propName]);
+//     }
+// }
+//  return array;
+
+//   }
+  
+
+//   console.log(getAllPropValues("name"));
+//   console.log(getAllPropValues("quantity"));
+//   console.log(getAllPropValues("price"));
+//   console.log(getAllPropValues("category"));
+//   =========================================================================================
+
+// Функція calculateTotalPrice(productName) 
+// приймає один параметр productName- назва товару. 
+// Функція містить масив об'єктів products з такими властивостями, 
+// як name — ім'я товару, price — ціна і quantity — кількість.
+
+// Доповни код функції так, щоб вона повертала загальну вартість (ціна * кількість) 
+// товару з таким ім'ям з масиву products.
+
+// Якщо продукту з такою назвою немає, то функція повинна повертати рядок 
+// "Product <productName> not found!" , де <productName> — це ім'я товару.
+
+// function calculateTotalPrice(productName) {
+//     const products = [
+//       { name: "Radar", price: 1300, quantity: 4 },
+//       { name: "Scanner", price: 2700, quantity: 3 },
+//       { name: "Droid", price: 400, quantity: 7 },
+//       { name: "Grip", price: 1200, quantity: 9 },
+//     ];
+    
+//     for (let i = 0; i < products.length; i ++){
+//         if (productName === products[i].name)
+//         return products[i].price * products[i].quantity;
+//     }
+//     return  `Product ${productName} not found!`   
+//   }
+
+//   console.log(calculateTotalPrice("Blaster"));
+//   console.log(calculateTotalPrice("Radar"));
+//   console.log(calculateTotalPrice("Droid"));
+//   console.log(calculateTotalPrice("Grip"));
+//   console.log(calculateTotalPrice("Scanner"));
+//   =====================================================================================================
