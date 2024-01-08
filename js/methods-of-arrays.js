4a780db84515b60f702225f907f2f5fdd22f4e45
TyshchenkoYulia <veshkina.yulia021088@gmail.com>
2024-01-08 14:06:55 +0200

    tasks

--- js/methods-of-arrays.js
+++ js/methods-of-arrays.js
@@ -153,3 +153,205 @@
 // console.log(filterArray([12, 24, 8, 41, 76], 20));
 // =========================================================================================================
 
+// Функція changeEven(numbers, value) приймає масив чисел numbers і оновлює кожен елемент, 
+// значення якого - це парне число, додаючи до нього значення параметра value, 
+// який точно є числом.
+
+// Виконай рефакторинг функції таким чином, щоб вона стала чистою - 
+// не змінювала масив чисел numbers, а створювала, 
+// наповнювала і повертала новий масив з оновленими значеннями.
+
+// function changeEven(numbers, value) {
+//     for (let i = 0; i < numbers.length; i += 1) {
+//       if (numbers[i] % 2 === 0) {
+//         numbers[i] = numbers[i] + value;
+//       }
+//     }
+//   }
+// ----------------------------------------------
+// const changeEven = (numbers, value) => {
+//     let newArray = [];
+
+//     numbers.forEach(number => {
+//         if (number % 2 === 0) {
+//             newArray.push(number + value);
+//         } else {
+//             newArray.push(number);
+//         }
+//     })
+
+//     return newArray;
+// }
+
+
+//   console.log(changeEven([1, 2, 3, 4, 5], 10));
+//   console.log(changeEven([2, 8, 3, 7, 4, 6], 10));
+//   console.log(changeEven([17, 24, 68, 31, 42], 100));
+//   console.log(changeEven([44, 13, 81, 92, 36, 54], 100));
+// ========================================================================================
+
+// В масиві planets зберігаються назви планет. Доповни код таким чином, 
+// щоб у змінній planetsLengths вийшов масив,
+//  що буде складатися з довжин назв кожної планети з масиву planets. 
+//  Обов'язково використовуй метод map().
+
+// const planets = ["Earth", "Mars", "Venus", "Jupiter"];
+
+// const planetsLengths = planets.map(planet => planet.length);
+ 
+
+// console.log(planetsLengths);
+// =====================================================================================
+
+// Масив books містить колекцію об'єктів книг, 
+// кожен з яких містить властивості title, author, rating. 
+// Використовуючи метод map(), зроби так, 
+// щоб у змінній titles вийшов масив назв усіх книг (властивість title) з масиву books.
+
+// const books = [
+//     {
+//       title: "The Last Kingdom",
+//       author: "Bernard Cornwell",
+//       rating: 8.38,
+//     },
+//     {
+//       title: "Beside Still Waters",
+//       author: "Robert Sheckley",
+//       rating: 8.51,
+//     },
+//     {
+//       title: "The Dream of a Ridiculous Man",
+//       author: "Fyodor Dostoevsky",
+//       rating: 7.75,
+//     },
+//     { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//     { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+//   ];
+  
+//    const titles = books.map(book => book.title);
+//     console.log(titles);
+
+    // ========================================================================================================
+
+// Масив books містить колекцію об'єктів книг, кожен з яких містить властивість genres, 
+//  значенням якої є масив жанрів. Використовуючи метод flatMap(), зроби так,
+//  щоб у змінній genres вийшов масив жанрів усіх книг (властивість genres) з масиву books.
+
+// const books = [
+//     {
+//       title: "The Last Kingdom",
+//       author: "Bernard Cornwell",
+//       genres: ["adventure", "history"],
+//     },
+//     {
+//       title: "Beside Still Waters",
+//       author: "Robert Sheckley",
+//       genres: ["fiction"],
+//     },
+//     {
+//       title: "Redder Than Blood",
+//       author: "Tanith Lee",
+//       genres: ["horror", "mysticism"],
+//     },
+//   ];
+  
+//   const genres = books.flatMap(book => book.genres);
+
+//   console.log(genres);
+// =============================================================================================================
+
+// Цей масив об'єктів ми будемо передавати в параметр users під час виклику функції із завдання.
+// Доповни стрілочну функцію getUserEmails(users) таким чином,
+// щоб вона повертала масив поштових адрес користувачів (властивість email) 
+// з масиву об'єктів в параметрі users.
+
+
+// const users = [
+//     {
+//       name: "Moore Hensley",
+//       email: "moorehensley@indexia.com",
+//       eyeColor: "blue",
+//       friends: ["Sharron Pace"],
+//       isActive: false,
+//       balance: 2811,
+//       skills: ["ipsum", "lorem"],
+//       gender: "male",
+//       age: 37,
+//     },
+//     {
+//       name: "Sharlene Bush",
+//       email: "sharlenebush@tubesys.com",
+//       eyeColor: "blue",
+//       friends: ["Briana Decker", "Sharron Pace"],
+//       isActive: true,
+//       balance: 3821,
+//       skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
+//       gender: "female",
+//       age: 34,
+//     },
+//     {
+//       name: "Ross Vazquez",
+//       email: "rossvazquez@xinware.com",
+//       eyeColor: "green",
+//       friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//       isActive: false,
+//       balance: 3793,
+//       skills: ["nulla", "anim", "proident", "ipsum", "elit"],
+//       gender: "male",
+//       age: 24,
+//     },
+//     {
+//       name: "Elma Head",
+//       email: "elmahead@omatom.com",
+//       eyeColor: "green",
+//       friends: ["Goldie Gentry", "Aisha Tran"],
+//       isActive: true,
+//       balance: 2278,
+//       skills: ["adipisicing", "irure", "velit"],
+//       gender: "female",
+//       age: 21,
+//     },
+//     {
+//       name: "Carey Barr",
+//       email: "careybarr@nurali.com",
+//       eyeColor: "blue",
+//       friends: ["Jordan Sampson", "Eddie Strong"],
+//       isActive: true,
+//       balance: 3951,
+//       skills: ["ex", "culpa", "nostrud"],
+//       gender: "male",
+//       age: 27,
+//     },
+//     {
+//       name: "Blackburn Dotson",
+//       email: "blackburndotson@furnigeer.com",
+//       eyeColor: "brown",
+//       friends: ["Jacklyn Lucas", "Linda Chapman"],
+//       isActive: false,
+//       balance: 1498,
+//       skills: ["non", "amet", "ipsum"],
+//       gender: "male",
+//       age: 38,
+//     },
+//     {
+//       name: "Sheree Anthony",
+//       email: "shereeanthony@kog.com",
+//       eyeColor: "brown",
+//       friends: ["Goldie Gentry", "Briana Decker"],
+//       isActive: true,
+//       balance: 2764,
+//       skills: ["lorem", "veniam", "culpa"],
+//       gender: "female",
+//       age: 39,
+//     },
+//   ];
+//   const getUserEmails = users.map(users => {
+//     return users.email;
+//   });
+//   const getUser = users.flatMap(users => {
+//     return users.email;
+//   });
+
+//   console.log(getUserEmails);
+//   console.log(getUser);
+//   =========================================================================================================
