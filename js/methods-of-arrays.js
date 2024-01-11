b20b0b274d4a5b7a9838135f6acc9fc299fdf1b6
TyshchenkoYulia <veshkina.yulia021088@gmail.com>
2024-01-11 19:44:57 +0200

    add

--- js/methods-of-arrays.js
+++ js/methods-of-arrays.js
@@ -355,3 +355,390 @@
 //   console.log(getUserEmails);
 //   console.log(getUser);
 //   =========================================================================================================
+// Доповни код так, щоб у змінній evenNumbers був масив парних чисел із масиву numbers,
+//  а у змінній oddNumbers — масив непарних. Обов'язково використовуй метод filter().
+
+// const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+
+// const evenNumbers = numbers.filter(number => number % 2 === 0);
+// const oddNumbers = numbers.filter(number => number % 2 );
+
+// console.log(evenNumbers);
+// console.log(oddNumbers);
+// =========================================================================================================
+
+// Масив books містить колекцію об'єктів книг, кожен з яких містить властивості title, author, rating.
+//  Використовуючи метод filter(), доповни код таким чином, щоб:
+
+// У змінній topRatedBooks утворився масив книг, рейтинг яких (властивість rating) 
+// більший за або дорівнює значенню змінної MIN_RATING.
+// У змінній booksByAuthor утворився масив книг, написаних автором з ім'ям (властивість author), 
+// яке збігається зі значенням у змінній AUTHOR.
+
+// const books = [
+//     {
+//       title: "The Last Kingdom",
+//       author: "Bernard Cornwell",
+//       rating: 8.38,
+//     },
+//     {
+//       title: "Beside Still Waters",
+//       author: "Robert Sheckley",
+//       rating: 8.51,
+//     },
+//     {
+//       title: "The Dream of a Ridiculous Man",
+//       author: "Fyodor Dostoevsky",
+//       rating: 7.75,
+//     },
+//     { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//     { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+//   ];
+  
+//   const MIN_RATING = 8;
+//   const AUTHOR = "Bernard Cornwell";
+  
+//   const topRatedBooks = books.filter(books => books.rating >= MIN_RATING);
+//   const booksByAuthor = books.filter(books => books.author === AUTHOR);
+
+//   console.log(topRatedBooks);
+//   console.log(booksByAuthor);
+//   ==================================================================================================
+// Доповни функцію getUsersWithEyeColor(users, color) таким чином, 
+// щоб вона повертала масив користувачів,
+//  у яких колір очей (властивість eyeColor) збігається зі 
+//  значенням другого параметра color.
+
+// const users = [
+//     {
+//       name: "Moore Hensley",
+//       email: "moorehensley@indexia.com",
+//       eyeColor: "blue",
+//       friends: ["Sharron Pace"],
+//       isActive: false,
+//       balance: 2811,
+//       gender: "male"
+//     },
+//     {
+//       name: "Sharlene Bush",
+//       email: "sharlenebush@tubesys.com",
+//       eyeColor: "blue",
+//       friends: ["Briana Decker", "Sharron Pace"],
+//       isActive: true,
+//       balance: 3821,
+//       gender: "female"
+//     },
+//     {
+//       name: "Ross Vazquez",
+//       email: "rossvazquez@xinware.com",
+//       eyeColor: "green",
+//       friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//       isActive: false,
+//       balance: 3793,
+//       gender: "male"
+//     },
+//     {
+//       name: "Elma Head",
+//       email: "elmahead@omatom.com",
+//       eyeColor: "green",
+//       friends: ["Goldie Gentry", "Aisha Tran"],
+//       isActive: true,
+//       balance: 2278,
+//       gender: "female"
+//     },
+//     {
+//       name: "Carey Barr",
+//       email: "careybarr@nurali.com",
+//       eyeColor: "blue",
+//       friends: ["Jordan Sampson", "Eddie Strong"],
+//       isActive: true,
+//       balance: 3951,
+//       gender: "male"
+//     },
+//     {
+//       name: "Blackburn Dotson",
+//       email: "blackburndotson@furnigeer.com",
+//       eyeColor: "brown",
+//       friends: ["Jacklyn Lucas", "Linda Chapman"],
+//       isActive: false,
+//       balance: 1498,
+//       gender: "male"
+//     },
+//     {
+//       name: "Sheree Anthony",
+//       email: "shereeanthony@kog.com",
+//       eyeColor: "brown",
+//       friends: ["Goldie Gentry", "Briana Decker"],
+//       isActive: true,
+//       balance: 2764,
+//       gender: "female"
+//     }
+//   ]
+//   const getUsersWithEyeColor = (users, color) => {
+//     return users.filter(users => users.eyeColor === color)
+//   };
+// ==================================================================================================
+// Доповни код функції getUsersWithAge(users, minAge, maxAge), 
+// щоб повернути масив користувачів, 
+// вік (збережений у властивості age) яких потрапляє
+//  у заданий діапазон від minAge до maxAge.
+
+// Поради:
+
+// Використай метод filter() для створення нового масиву, 
+// в якому зберігаються тільки елементи, які задовольняють певну умову.
+// Використай оператори >= (більше або дорівнює) та <= (менше або дорівнює), 
+// щоб відфільтрувати користувачів, вік яких точно потрапляє у діапазон між мінімальним minAge 
+// та максимальним maxAge значеннями.
+// const users = [
+//     {
+//       name: "Moore Hensley",
+//       email: "moorehensley@indexia.com",
+//       eyeColor: "blue",
+//       friends: ["Sharron Pace"],
+//       isActive: false,
+//       balance: 2811,
+//       gender: "male",
+//       age: 37
+//     },
+//     {
+//       name: "Sharlene Bush",
+//       email: "sharlenebush@tubesys.com",
+//       eyeColor: "blue",
+//       friends: ["Briana Decker", "Sharron Pace"],
+//       isActive: true,
+//       balance: 3821,
+//       gender: "female",
+//       age: 34
+//     },
+//     {
+//       name: "Ross Vazquez",
+//       email: "rossvazquez@xinware.com",
+//       eyeColor: "green",
+//       friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//       isActive: false,
+//       balance: 3793,
+//       gender: "male",
+//       age: 24
+//     },
+//     {
+//       name: "Elma Head",
+//       email: "elmahead@omatom.com",
+//       eyeColor: "green",
+//       friends: ["Goldie Gentry", "Aisha Tran"],
+//       isActive: true,
+//       balance: 2278,
+//       gender: "female",
+//       age: 21
+//     },
+//     {
+//       name: "Carey Barr",
+//       email: "careybarr@nurali.com",
+//       eyeColor: "blue",
+//       friends: ["Jordan Sampson", "Eddie Strong"],
+//       isActive: true,
+//       balance: 3951,
+//       gender: "male",
+//       age: 27
+//     },
+//     {
+//       name: "Blackburn Dotson",
+//       email: "blackburndotson@furnigeer.com",
+//       eyeColor: "brown",
+//       friends: ["Jacklyn Lucas", "Linda Chapman"],
+//       isActive: false,
+//       balance: 1498,
+//       gender: "male",
+//       age: 38
+//     },
+//     {
+//       name: "Sheree Anthony",
+//       email: "shereeanthony@kog.com",
+//       eyeColor: "brown",
+//       friends: ["Goldie Gentry", "Briana Decker"],
+//       isActive: true,
+//       balance: 2764,
+//       gender: "female",
+//       age: 39
+//     }
+//   ]
+// const getUsersWithAge = (users, minAge, maxAge) => {
+//    return users.filter(users => users.age >= minAge && users.age <= maxAge)
+// };
+  
+//   console.log(getUsersWithAge(users, 20,30));
+//   ================================================================================================
+
+// Масив books містить колекцію об'єктів книг, кожен з яких містить властивості title, author, rating.
+
+// Використовуючи метод find(), доповни код таким чином, щоб:
+
+// У змінній bookWithTitle утворився об'єкт книги, назва якої (властивість title) 
+// збігається зі значенням змінної BOOK_TITLE.
+// У змінній bookByAuthor утворився об'єкт книги, автор якої (властивість author) 
+// збігається зі значенням змінної AUTHOR.
+
+// const books = [
+//     {
+//       title: "The Last Kingdom",
+//       author: "Bernard Cornwell",
+//       rating: 8.38,
+//     },
+//     {
+//       title: "Beside Still Waters",
+//       author: "Robert Sheckley",
+//       rating: 8.51,
+//     },
+//     {
+//       title: "The Dream of a Ridiculous Man",
+//       author: "Fyodor Dostoevsky",
+//       rating: 7.75,
+//     },
+//     { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//   ];
+//   const BOOK_TITLE = "The Dream of a Ridiculous Man";
+//   const AUTHOR = "Robert Sheckley";
+  
+//   const bookWithTitle = books.find(books => books.title === BOOK_TITLE);
+//   const bookByAuthor = books.find(books => books.author === AUTHOR);
+
+//   console.log(bookWithTitle);
+//   console.log(bookByAuthor);
+// =============================================================================================
+// Доповни функцію getUserWithEmail(users, email) таким чином, 
+// щоб вона повертала об'єкт користувача, пошта якого (властивість email) 
+// збігається зі значенням другого параметра email.
+
+// const users = [
+//     {
+//       name: "Moore Hensley",
+//       email: "moorehensley@indexia.com",
+//       eyeColor: "blue",
+//       friends: ["Sharron Pace"],
+//       isActive: false,
+//       balance: 2811,
+//       gender: "male"
+//     },
+//     {
+//       name: "Sharlene Bush",
+//       email: "sharlenebush@tubesys.com",
+//       eyeColor: "blue",
+//       friends: ["Briana Decker", "Sharron Pace"],
+//       isActive: true,
+//       balance: 3821,
+//       gender: "female"
+//     },
+//     {
+//       name: "Ross Vazquez",
+//       email: "rossvazquez@xinware.com",
+//       eyeColor: "green",
+//       friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//       isActive: false,
+//       balance: 3793,
+//       gender: "male"
+//     },
+//     {
+//       name: "Elma Head",
+//       email: "elmahead@omatom.com",
+//       eyeColor: "green",
+//       friends: ["Goldie Gentry", "Aisha Tran"],
+//       isActive: true,
+//       balance: 2278,
+//       gender: "female"
+//     },
+//     {
+//       name: "Carey Barr",
+//       email: "careybarr@nurali.com",
+//       eyeColor: "blue",
+//       friends: ["Jordan Sampson", "Eddie Strong"],
+//       isActive: true,
+//       balance: 3951,
+//       gender: "male"
+//     },
+//     {
+//       name: "Blackburn Dotson",
+//       email: "blackburndotson@furnigeer.com",
+//       eyeColor: "brown",
+//       friends: ["Jacklyn Lucas", "Linda Chapman"],
+//       isActive: false,
+//       balance: 1498,
+//       gender: "male"
+//     },
+//     {
+//       name: "Sheree Anthony",
+//       email: "shereeanthony@kog.com",
+//       eyeColor: "brown",
+//       friends: ["Goldie Gentry", "Briana Decker"],
+//       isActive: true,
+//       balance: 2764,
+//       gender: "female"
+//     }
+//   ]
+
+//   const getUserWithEmail = (users, email) => {
+//     return users.find(users => users.email === email)
+//   };
+//   console.log(getUserWithEmail(users, "shereeanthony@kog.com"));
+//   console.log(getUserWithEmail(users, "blackburndotson@furnigeer.com"));
+//   console.log(getUserWithEmail(users));
+// ===================================================================================================
+// Ігровому сервісу необхідний функціонал підрахунку середнього часу,
+//  проведеного в іграх, одним гравцем. У змінній players зберігається об'єкт, де ключ це ім'я гравця,
+//   а значення - його ігровий час. У змінній playtimes зберігається масив значень об'єкта players, 
+//   тобто масив ігрового часу усіх гравців. Значенням змінної averagePlayTime буде середній час, 
+//   проведений одним гравцем в іграх.
+
+// Доповни код таким чином, щоб у змінній totalPlayTime вийшов 
+// загальний ігровий час з масиву playtimes.
+//  Використовуй метод reduce().
+
+// const players = {
+//     mango: 1270,
+//     poly: 468,
+//     ajax: 710,
+//     kiwi: 244,
+//   };
+//   const playtimes = Object.values(players); // [1270, 468, 710, 244]
+  
+//   const totalPlayTime = playtimes.reduce((previousValue, number) => 
+//   previousValue += number);
+  
+//   const averagePlayTime = totalPlayTime / playtimes.length;
+
+//   console.log(totalPlayTime);
+//   console.log(averagePlayTime);
+
+// =================================================================================================
+
+// У змінній players зберігається масив об'єктів, кожен з яких має властивості name, playtime та gamesPlayed.
+
+// Нашому сервісу необхідно розрахувати середній час, проведений в одній грі для кожного гравця,
+//  і отримати загальну суму цих значень часу у змінній totalAveragePlaytimePerGame. 
+//  Розрахувати час для кожного з гравців можна, розділивши його час (властивість playtime) 
+//  на кількість ігор (властивість gamesPlayed).
+
+// Поради:
+
+// Використовуй метод reduce() для ітерації по масиву players та обчислення 
+// загальної суми середнього часу на одну гру.
+// Усередині колбек функції reduce(), поділи playtime гравця на gamesPlayed, 
+// щоб отримати середній час, витрачений на одну гру кожним гравцем.
+// Накопичуй результат у змінній acc і повертай його в кінці кожної ітерації.
+// Ініціалізуй параметр acc методу reduce() початковим значенням 0, 
+// щоб уникнути отримання NaN при виконанні обчислень.
+// В результаті змінна totalAveragePlaytimePerGame міститиме 
+// загальну суму середнього часу на одну гру для всіх гравців.
+
+const players = [
+    { name: "Mango", playtime: 1270, gamesPlayed: 4 },
+    { name: "Poly", playtime: 469, gamesPlayed: 2 },
+    { name: "Ajax", playtime: 690, gamesPlayed: 3 },
+    { name: "Kiwi", playtime: 241, gamesPlayed: 1 },
+  ];
+  
+  const totalAveragePlaytimePerGame = players.reduce(
+    (total, players)  => 
+       total + players.playtime / players.gamesPlayed
+    , 0);
+  
+console.log(totalAveragePlaytimePerGame);
