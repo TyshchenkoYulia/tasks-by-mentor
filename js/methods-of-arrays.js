6d918f502c4ac0f0d2775eb6080a69006e5be825
TyshchenkoYulia <veshkina.yulia021088@gmail.com>
2024-01-12 17:52:15 +0200

    add

--- js/methods-of-arrays.js
+++ js/methods-of-arrays.js
@@ -729,16 +729,172 @@
 // В результаті змінна totalAveragePlaytimePerGame міститиме 
 // загальну суму середнього часу на одну гру для всіх гравців.
 
-const players = [
-    { name: "Mango", playtime: 1270, gamesPlayed: 4 },
-    { name: "Poly", playtime: 469, gamesPlayed: 2 },
-    { name: "Ajax", playtime: 690, gamesPlayed: 3 },
-    { name: "Kiwi", playtime: 241, gamesPlayed: 1 },
-  ];
+// const players = [
+//     { name: "Mango", playtime: 1270, gamesPlayed: 4 },
+//     { name: "Poly", playtime: 469, gamesPlayed: 2 },
+//     { name: "Ajax", playtime: 690, gamesPlayed: 3 },
+//     { name: "Kiwi", playtime: 241, gamesPlayed: 1 },
+//   ];
   
-  const totalAveragePlaytimePerGame = players.reduce(
-    (total, players)  => 
-       total + players.playtime / players.gamesPlayed
-    , 0);
+//   const totalAveragePlaytimePerGame = players.reduce(
+//     (total, players)  => 
+//        total + players.playtime / players.gamesPlayed
+//     , 0);
   
-console.log(totalAveragePlaytimePerGame);
+// console.log(totalAveragePlaytimePerGame);
+// ================================================================================================
+
+// Змінна releaseDates - це масив чисел, років видання книг. 
+// Змінна authors - це масив рядків, авторів книг.
+
+// Доповни код таким чином, щоб у змінній ascendingReleaseDates вийшла копія масиву releaseDates, 
+// відсортована за зростанням, а у змінній alphabeticalAuthors - копія масиву імен авторів authors, 
+// відсортована за алфавітом. Використовуй метод toSorted()
+
+// const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
+// const authors = [
+//   "Tanith Lee",
+//   "Bernard Cornwell",
+//   "Robert Sheckley",
+//   "Fyodor Dostoevsky",
+// ];
+
+// const ascendingReleaseDates = releaseDates.toSorted();
+
+// const alphabeticalAuthors = authors.toSorted();
+
+// console.log(ascendingReleaseDates);
+// console.log(alphabeticalAuthors);
+// ==================================================================================================
+// Змінна releaseDates - це масив чисел, років видання книг.
+
+// Онлайн бібліотеці необхідно відображати книги, відсортовані за датою видання,
+//  за їх зростанням або спаданням. Доповни код таким чином, 
+//  щоб у змінній ascendingReleaseDates вийшла копія масиву releaseDates, відсортована за зростанням,
+//  а у змінній descendingReleaseDates - копія, відсортована за спаданням.
+
+// const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
+
+// const ascendingReleaseDates = releaseDates.toSorted((a, b) => a - b);
+
+// const descendingReleaseDates = releaseDates.toSorted((a, b) => b - a);
+// console.log(ascendingReleaseDates);
+// console.log(descendingReleaseDates);
+// =======================================================================================================
+// Змінна authors - це масив рядків, авторів книг.
+
+// Онлайн бібліотеці необхідно відображати книги, 
+// відсортовані за автором в алфавітному і в зворотному алфавітному порядку. 
+// Доповни код таким чином, щоб у змінній authorsInAlphabetOrder вийшла копія масиву authors, 
+// відсортована за алфавітом, а у змінній authorsInReversedOrder - копія, 
+// відсортована у зворотному алфавітному порядку.
+
+// const authors = [
+//   "Tanith Lee",
+//   "Bernard Cornwell",
+//   "Robert Sheckley",
+//   "Fyodor Dostoevsky",
+//   "Howard Lovecraft",
+// ];
+
+// const authorsInAlphabetOrder = authors.toSorted((a, b) => a.localeCompare(b));
+
+// const authorsInReversedOrder = authors.toSorted((a, b) => b.localeCompare(a));
+
+// console.log(authorsInAlphabetOrder);
+// console.log(authorsInReversedOrder);
+// ===========================================================================================================
+// Масив books містить масив об'єктів книг, кожен з яких містить властивості title, author, rating.
+
+// Доповни код таким чином, щоб:
+
+// У змінній sortedByAuthorName вийшов масив книг, 
+// відсортований за ім'ям автора в алфавітному порядку.
+// У змінній sortedByReversedAuthorName вийшов масив книг, 
+// відсортований за ім'ям автора у зворотному алфавітному порядку.
+// У змінній sortedByAscendingRating вийшов масив книг, відсортований за зростанням рейтингу.
+// У змінній sortedByDescentingRating вийшов масив книг, відсортований за спаданням рейтингу.
+
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+//   {
+//     title: "Redder Than Blood",
+//     author: "Tanith Lee",
+//     rating: 7.94,
+//   },
+//   {
+//     title: "Enemy of God",
+//     author: "Bernard Cornwell",
+//     rating: 8.67,
+//   },
+// ];
+
+// const sortedByAuthorName = books.toSorted((a, b) => a.author.localeCompare(b.author));
+
+// const sortedByReversedAuthorName = books.toSorted((a, b) => b.author.localeCompare(a.author));
+
+// const sortedByAscendingRating = books.toSorted((a, b) => a.rating - b.rating);
+
+// const sortedByDescentingRating = books.toSorted((a, b) => b.rating - a.rating);
+
+// console.log(sortedByAuthorName);
+// console.log(sortedByReversedAuthorName);
+// console.log(sortedByAscendingRating);
+// console.log(sortedByDescentingRating);
+// ===========================================================================================================
+// Масив books містить масив об'єктів книг, 
+// кожен з яких містить властивості title, author, rating.
+
+// Доповни код таким чином, щоб у змінній names вийшов
+//  масив імен авторів в алфавітному порядку,
+//  рейтинг книг яких більший за значення змінної MIN_BOOK_RATING. 
+//  Використовуй ланцюжок методів.
+
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+//   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//   {
+//     title: "The Dreams in the Witch House",
+//     author: "Howard Lovecraft",
+//     rating: 8.67,
+//   },
+// ];
+// const MIN_BOOK_RATING = 8;
+
+// const names = books
+  
+//   .filter(book => book.rating > MIN_BOOK_RATING)
+//   .map(book => book.author)
+//   .toSorted((a, b) => a.localeCompare(b))
+//   ;
+
+
+//   console.log(names);
