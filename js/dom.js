84fd0be51ea1ea5a0a80230f023c668170783d7b
TyshchenkoYulia <145122847+TyshchenkoYulia@users.noreply.github.com>
2024-01-26 22:47:29 +0200

    add

---
+++ js/dom.js
@@ -0,0 +1,49 @@
+/*
+Завдання 2
+Після натискання кнопки "SWAP ME" здійснюється 
+обмін вмістом між двома інпутами.
+Ви можете натиснути на неї кілька разів або вручну змінити вміст інпутів.
+*/
+// const swapButton = document.querySelector("#swapButton");
+
+// const leftSwapInput = document.querySelector("#leftSwapInput");
+// const rightSwapInput = document.querySelector("#rightSwapInput");
+
+
+
+// swapButton.addEventListener('click', onSwapButtonClick);
+
+// function onSwapButtonClick() {
+
+//     const leftSwapInputValue = leftSwapInput.value;
+//     leftSwapInput.value = rightSwapInput.value;
+//     rightSwapInput.value = leftSwapInputValue;
+
+// }
+// =========================================================================
+
+/*
+Завдання 3
+Кнопка "Приховати" ховає текст і замінює назву кнопки на
+"Розкрити", при повторному натисканні текст знову стає доступним
+і кнопка набуває початкового вигляду.
+*/
+
+// const passwordButton = document.querySelector("#passwordButton");
+// const taskTitle = document.querySelector(".taskTitle");
+
+// passwordButton.addEventListener('click', onPasswordButtonClick);
+
+// function onPasswordButtonClick() {
+
+//  if (passwordButton.textContent === "Open") {
+//          taskTitle.style.display = "block";
+//      passwordButton.textContent = "Close";
+//      return;
+//     }
+
+//     taskTitle.style.display = "none";
+//     passwordButton.textContent = "Open";
+
+// }
+// ==========================================================================
