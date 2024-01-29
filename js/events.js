e79ad0c198724657605bf082bc7e9e83c718e7d0
TyshchenkoYulia <veshkina.yulia021088@gmail.com>
2024-01-29 16:43:00 +0200

    add

--- js/events.js
+++ js/events.js
@@ -1,6 +1,28 @@
-const parent = document.querySelector("#parent");
+// const parent = document.querySelector("#parent");
 
-parent.addEventListener("click", (event) => {
-  console.log("event.target: ", event.target);
-  console.log("event.currentTarget: ", event.currentTarget);
-});
+// parent.addEventListener("click", (event) => {
+//   console.log("event.target: ", event.target);
+//   console.log("event.currentTarget: ", event.currentTarget);
+// });
+
+
+// ================================================================
+
+{/* <div class="box">
+<button>Button 1</button>
+<button>Button 2</button>
+<button>Button 3</button>
+<button>Button 4</button>
+<button>Button 5</button>
+<button>Button 6</button>
+<button>Button 7</button>
+<button>Button 8</button>
+</div> */}
+
+
+// const box = document.querySelector("div.box");
+
+// box.addEventListener("click", function (event) {
+//   console.log(event.target);
+// });
+// ===============================================================
